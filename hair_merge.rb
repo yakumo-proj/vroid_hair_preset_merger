7aa7bcb4204fbdf46ead722ba6e7ed9b4272b443
Yakumo Sayo <63381002+yakumo-proj@users.noreply.github.com>
2020-05-13 06:56:50 +0900

    Update and rename hair_marge.rb to hair_merge.rb

--- hair_merge.rb
+++ hair_merge.rb
@@ -1,6 +1,6 @@
-# hair_presets marge tools ver.0.1 (GNU GPLv3 ver.)
+# hair_presets merge tools ver.0.1 (GNU GPLv3 ver.)
 # Copyright (C) 2020  Yakumo Sayo, Susanoo Lab. All rights reserved.
-# usage: ruby hair_marge.rb presetXXX presetYYY
+# usage: ruby hair_merge.rb presetXXX presetYYY
 #  => directory 'presetXXX' marged.
 
 #!/usr/bin/ruby
@@ -18,4 +18,4 @@ File.open(ARGV[0] + '/preset.json', mode = 'w') {|f| f.write(JSON.generate(x)) }
 
 #copy textures
 textures = "/materials/rendered_textures/"
-FileUtils.cp_r(  Dir.glob(ARGV[1] + textures + "*"), ARGV[0] + textures )
+FileUtils.cp_r(Dir.glob(ARGV[1] + textures + "*"), ARGV[0] + textures)
